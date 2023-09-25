9e7ae26caecf503d4ae3062f8ed4058242658670
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-09-25 16:34:23 +0200

    Update Description1.jsx

--- src/Description1.jsx
+++ src/Description1.jsx
@@ -2,6 +2,6 @@ export default function DescPatterns1() {
   return (
     <div className="game_description">
       <h6>Oscillators</h6>
-      <p>These are all examples of so called <span className="bold">oscillators</span> (i.e. patterns that returns to their original state, after a finite number of generations. The ones in the upper left are named <span className="bold">Toad</span> and <span className="bold">Beacon</span>, each of period 2. The figure in the lower left is called <span className="bold">Pulsar</span> and has a period of 3. On the upper right you find the <span className="bold">Pentadecathlon</span> of period 15. The pattern in the lower right is called <span className="bold">'Figure eight' </span>(period 8).</p>
+      <p>These are all examples of so called <span className="bold">oscillators</span> (i.e. patterns that returns to their original state, after a finite number of generations). The ones in the upper left are named <span className="bold">Toad</span> and <span className="bold">Beacon</span>, each of period 2. The figure in the lower left is called <span className="bold">Pulsar</span> and has a period of 3. On the upper right you find the <span className="bold">Pentadecathlon</span> of period 15. The pattern in the lower right is called <span className="bold">'Figure eight' </span>(period 8).</p>
 </div>)
 }
